504d1f5d8e9000bc8ba7f53621e3b3c1cc84f15b
ValentinChanter <singvalent@cy-tech.fr>
2023-11-10 17:03:38 +0100

    No more tests

--- src/main/java/com/cytech/marketplace/Main.java
+++ src/main/java/com/cytech/marketplace/Main.java
@@ -1,9 +1,11 @@
 package com.cytech.marketplace;
 
 import com.cytech.marketplace.dao.UsersDAO;
+import com.cytech.marketplace.entity.Users;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(UsersDAO.login("phoenix.wright@gmail.com", "1234"));
+
+
     }
 }
