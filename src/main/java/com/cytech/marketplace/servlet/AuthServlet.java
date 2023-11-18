24bce396b11b655848a93431370707b5edf00c2f
ValentinChanter <singvalent@cy-tech.fr>
2023-11-18 16:30:51 +0100

    Merge session and db cart on login

--- src/main/java/com/cytech/marketplace/servlet/AuthServlet.java
+++ src/main/java/com/cytech/marketplace/servlet/AuthServlet.java
@@ -1,6 +1,8 @@
 package com.cytech.marketplace.servlet;
 
 import com.cytech.marketplace.dao.UsersDAO;
+import com.cytech.marketplace.entity.Articles;
+import com.cytech.marketplace.entity.Users;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,13 +10,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Map;
 
 @WebServlet(name = "authServlet", value = "/auth-servlet")
 public class AuthServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if (UsersDAO.login(req.getParameter("email"), req.getParameter("password"))) {
-            req.getSession().setAttribute("user", UsersDAO.getUser(req.getParameter("email")));
+            Users users = UsersDAO.getUser(req.getParameter("email"));
+            assert users != null;
+
+            req.getSession().setAttribute("user", users);
+
+            Map<Articles, Integer> cart = CartUtil.getCart(req);
+            if (!cart.isEmpty() && !UsersDAO.getCart(users).isEmpty()) {
+                CartUtil.mergeCart(req, users);
+            } else if (!cart.isEmpty()) {
+                UsersDAO.setCart(users, cart);
+            } else if (!UsersDAO.getCart(users).isEmpty()) {
+                req.getSession().setAttribute("cart", UsersDAO.getCart(users));
+            }
+
             resp.sendRedirect(getServletContext().getContextPath() + "/home");
         } else {
             req.setAttribute("error", "Email ou mot de passe incorrect. Veuillez réessayer.");
