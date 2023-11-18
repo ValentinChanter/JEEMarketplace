30b8871b40f779ccfb4967d2ff19a7b21d69583a
ValentinChanter <singvalent@cy-tech.fr>
2023-11-18 19:29:39 +0100

    Changing quantities in cart now updates cart

--- src/main/java/com/cytech/marketplace/servlet/CheckStockPostCartServlet.java
+++ src/main/java/com/cytech/marketplace/servlet/CheckStockPostCartServlet.java
@@ -1,6 +1,9 @@
 package com.cytech.marketplace.servlet;
 
 import com.cytech.marketplace.dao.ArticlesDAO;
+import com.cytech.marketplace.dao.UsersDAO;
+import com.cytech.marketplace.entity.Articles;
+import com.cytech.marketplace.entity.Users;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,6 +11,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
@@ -16,15 +20,34 @@ public class CheckStockPostCartServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Set<String> parameterNames = req.getParameterMap().keySet();
+        Users users = (Users) req.getSession().getAttribute("user");
+        Map<Articles, Integer> cart = CartUtil.getCart(req);
 
         for (String parameterName : parameterNames) {
             String value = req.getParameter(parameterName);
             String name = parameterName.substring(0, parameterName.indexOf("-input"));
+            Articles article = ArticlesDAO.getArticle(UUID.fromString(name));
+
+            if (Integer.parseInt(value) == 0) {
+                cart.remove(article);
+                continue;
+            }
+
             if (!ArticlesDAO.checkStock(UUID.fromString(name), Integer.parseInt(value))) {
                 req.setAttribute("error", "Une erreur de stock est survenue. Veuillez réessayer.");
                 req.getRequestDispatcher("/WEB-INF/view/cart.jsp").forward(req, resp);
                 return;
             }
+
+            for (Map.Entry<Articles, Integer> entry : cart.entrySet()) {
+                if (entry.getKey().equals(article)) {
+                    entry.setValue(Integer.parseInt(value));
+                }
+            }
+        }
+
+        if (users != null) {
+            UsersDAO.setCart(users, cart);
         }
 
         resp.sendRedirect(getServletContext().getContextPath() + "/infopersonnal");
