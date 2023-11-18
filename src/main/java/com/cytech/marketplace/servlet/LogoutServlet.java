6cc86fef704d4f4dfa402d138d2b013c8ba75e1e
ValentinChanter <singvalent@cy-tech.fr>
2023-11-18 16:32:15 +0100

    Empty session cart on logout

--- src/main/java/com/cytech/marketplace/servlet/LogoutServlet.java
+++ src/main/java/com/cytech/marketplace/servlet/LogoutServlet.java
@@ -1,17 +1,27 @@
 package com.cytech.marketplace.servlet;
 
+import com.cytech.marketplace.entity.Articles;
+import com.cytech.marketplace.entity.Users;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Map;
 
 @WebServlet(name = "logoutServlet", value = "/logout-servlet")
 public class LogoutServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        if (req.getSession().getAttribute("user") != null) {
+        Users users = (Users) req.getSession().getAttribute("user");
+        Map<Articles, Integer> cart = CartUtil.getCart(req);
+
+        if (users != null) {
+            if (!cart.isEmpty()) {
+                req.getSession().removeAttribute("cart");
+            }
+
             req.getSession().removeAttribute("user");
         }
 
