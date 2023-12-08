465b2645db52ee1d9474d3a58ad205e540de730d
LineFourcher <fourcherline@gmail.com>
2023-12-08 15:15:07 +0100

    added attribute with the quantity of the product in cart

--- src/main/java/com/cytech/marketplace/controller/ProductPageController.java
+++ src/main/java/com/cytech/marketplace/controller/ProductPageController.java
@@ -1,5 +1,6 @@
 package com.cytech.marketplace.controller;
 
+import com.cytech.marketplace.utils.CartUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,6 +11,7 @@ import com.cytech.marketplace.dao.ArticlesDAO;
 import com.cytech.marketplace.entity.Articles;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.UUID;
 
 @WebServlet(name = "productPageController", value = "/productPage")
@@ -17,9 +19,18 @@ public class ProductPageController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         String productId = req.getParameter("id");
+        Map<Articles, Integer> cart = CartUtil.getCart(req);
+
 
         if (productId != null && !productId.isEmpty()) {
             Articles product = ArticlesDAO.getArticle(UUID.fromString(productId));
+
+            if (cart.containsKey(product)) {
+                int qtyInCart = cart.get(product);
+                req.setAttribute("qty", qtyInCart);
+            } else {
+                req.setAttribute("qty", 0);
+            }
 
             req.setAttribute("product", product);
             req.getRequestDispatcher("/WEB-INF/view/productPage.jsp").forward(req, resp);
