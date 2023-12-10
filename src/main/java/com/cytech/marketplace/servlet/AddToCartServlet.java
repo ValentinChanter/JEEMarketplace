af033cdd0893f34d817a80913ac7e42eb2440e91
LineFourcher <fourcherline@gmail.com>
2023-12-10 14:08:10 +0100

    added extra verification of stock before adding to cart

--- src/main/java/com/cytech/marketplace/servlet/AddToCartServlet.java
+++ src/main/java/com/cytech/marketplace/servlet/AddToCartServlet.java
@@ -10,19 +10,35 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Map;
 import java.util.UUID;
+
+import static java.lang.Math.abs;
+
 
 @WebServlet(name = "addToCartServlet", value = "/addToCart-servlet")
 public class AddToCartServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         String productId = req.getParameter("productId");
         String productQty = req.getParameter("productQty");
+        Map<Articles, Integer> cart = CartUtil.getCart(req);
 
         UUID productIdUUID = UUID.fromString(productId);
         int qty = Integer.parseInt(productQty);
         Articles product = ArticlesDAO.getArticle(productIdUUID);
-
-        CartUtil.addArticleToCart(req, product, qty);
+        if (cart.containsKey(product)) {
+            int qtyInCart = cart.get(product);
+            int toPutInCart = qty + qtyInCart;
+            if (product.getStock().compareTo(BigInteger.valueOf(toPutInCart)) < 0){
+                int correctedQty = abs(qtyInCart - product.getStock().intValue());
+                CartUtil.addArticleToCart(req, product, correctedQty);
+            } else {
+            CartUtil.addArticleToCart(req, product, qty);
+            }
+        } else {
+            CartUtil.addArticleToCart(req, product, qty);
+        }
         req.getRequestDispatcher("/WEB-INF/view/cart.jsp").forward(req, resp);
     }
 }
