24f66526b65ad01b9e1d220580a8114c1fd971f3
guillermfr <guillerm.fya@orange.fr>
2023-12-10 01:50:25 +0100

    Added a controller for the fidelityPointsManagement page

---
+++ src/main/java/com/cytech/marketplace/controller/FidelityPointsManagementController.java
@@ -0,0 +1,17 @@
+package com.cytech.marketplace.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "fidelityPointsManagementController", value = "/fidelityPointsManagement")
+public class FidelityPointsManagementController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        req.getRequestDispatcher("/WEB-INF/view/fidelityPointsManagement.jsp").forward(req, resp);
+    }
+}
